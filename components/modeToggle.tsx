c68efff23c6b172a3f965b930234f4a96688b687
Ashok Kumar <ashokgupta2678@gmail.com>
2025-06-07 08:22:17 +0530

    added the shadcn

---
+++ components/modeToggle.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import * as React from "react"
+import { Moon, Sun } from "lucide-react"
+import { useTheme } from "next-themes"
+
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+export function ModeToggle() {
+  const { theme , setTheme } = useTheme()
+
+  const Toggle = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  }
+
+  return (
+    <Button variant="secondary" size="icon" className=" bg-transparent" onClick={Toggle}>
+      {theme === "light" ? (
+        <Sun className="h-[1.2rem] w-[1.2rem]" />
+      ) : (
+        <Moon className="h-[1.2rem] w-[1.2rem]" />
+      )}
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  )
+}
