b0aaef6ec44da604206626b8aec587223451e685
Ashok Kumar <ashokgupta2678@gmail.com>
2025-06-19 05:22:42 +0530

    added the custom 404

---
+++ app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function home(){
+    return (
+        <div className="min-h-screen flex flex-col justify-center items-center text-center">
+            <h1 className="text-4xl font-extrabold"> work is still in progress </h1>
+        </div>
+    )
+}
